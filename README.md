d1cfe4529f57539f9d9350d3ce55c8b4b0517b3f
Sankavi-Raveendran <sankaviraveendran@gmail.com>
2025-02-12 13:43:02 +0530

    todoapp finished

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # to-do-app
+# PHP To-Do List Project with MySQL Database
+
+##  Prerequisites
+Before running this project, ensure you have the following installed:
+- [XAMPP](https://www.apachefriends.org/) (Apache, MySQL, PHP)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- A web browser (Chrome, Firefox, etc.)
+
+---
+
+##  How to Run This Project
+
+### 1⃣ Start XAMPP
+1. Open **XAMPP Control Panel**.
+2. Start **Apache** and **MySQL**.
+
+---
+
+### 2⃣ Setup the Database
+1. Open your web browser and go to **http://localhost/phpmyadmin/**.
+2. Click on **Databases** and create a new database:
+   ```sql
+   CREATE DATABASE to_do_list;
+   ```
+3. Select your new database and go to the **Import** tab.
+4. Click **Choose File**, navigate to the **/database** folder, and select **to_do_list.sql**.
+5. Click **Import** to restore the database.
+
+---
+
+### 3⃣ Configure the Project
+1. Place the project folder inside **htdocs**:
+   ```
+   C:\xampp\htdocs\to_do_list\
+   ```
+
+---
+
+### 4⃣ Run the Project
+1. Open **VS Code** and go to **File → Open Folder** → Select `C:\xampp\htdocs\to_do_list\`.
+2. In the browser, open:
+   ```
+   http://localhost/to-do-list/to-do-app/
+   ```
+3. You should see the homepage of the To-Do List project.
+
+---
+
---
+++ todos.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 12, 2025 at 09:12 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `to_do_list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `checked` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `date_time`, `checked`) VALUES
+(6, 'Shopping grocery', '2025-02-12 13:40:21', 0),
+(8, 'Do laundry', '2025-02-12 13:41:15', 0),
+(9, 'Go walking', '2025-02-12 13:41:24', 0),
+(10, 'Do assignments', '2025-02-12 13:41:53', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
